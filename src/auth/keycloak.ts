391ed4adae1566819ed8a856a95e7315eb51802d
Riksa <dhimas.riksa@bssn.go.id>
2025-11-18 21:17:31 +0700

    update: keycloak

--- src/auth/keycloak.ts
+++ src/auth/keycloak.ts
@@ -3,7 +3,7 @@ import Keycloak from 'keycloak-js';
 
 const secret = import.meta.env.KCS;
 const keycloak = new Keycloak ({
-  url: 'https://auth.managementcorp.my.id/auth',
+  url: 'https://auth.managementcorp.my.id',
   realm: 'service',
   clientId: 'iphish',
 });
